f185d70311902e848862ade5742931959bb190be
Jonathan Jimenez <49576723+jonathanj101@users.noreply.github.com>
2025-06-08 02:39:26 -0400

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -128,7 +128,7 @@ def get_dealer_details(request, dealer_id):
 def add_review(request):
     if(request.user.is_anonymous == False):
         data = json.loads(request.body)
-        try:More actions
+        try:
             response = post_review(data)
             return JsonResponse({"status":200})
         except:
