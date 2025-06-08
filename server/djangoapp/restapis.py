50ccbb0dbd58f4da28d9642579ec0be72703c8fb
Jonathan Jimenez <49576723+jonathanj101@users.noreply.github.com>
2025-06-08 01:28:58 -0400

    Update restapis.py

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -31,7 +31,7 @@ def get_request(endpoint, **kwargs):
         # print(request_url)
         response = requests.get(request_url)
         return response.json()
-    except:
+    except Exception as err:
         # If any error occurs
         print("Network exception occurred")
 
@@ -56,5 +56,5 @@ def post_review(data_dict):
         response = requests.post(request_url,json=data_dict)
         print(response.json())
         return response.json()
-    except:
+    except Exception as err:
         print("Network exception occurred")
