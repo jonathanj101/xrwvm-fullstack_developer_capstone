e0993f847dbc66c60a283c5b51fa1c2589659899
Jonathan Jimenez <49576723+jonathanj101@users.noreply.github.com>
2025-06-08 02:41:08 -0400

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -65,13 +65,18 @@ def register(request):
 
 def get_cars(request):
     count = CarMake.objects.filter().count()
-    if(count == 0):
+    if (count == 0):
         initiate()
     car_models = CarModel.objects.select_related('car_make')
     print(car_models)
     cars = []
-    for car_model in car_models:
-        cars.append({"CarModel": car_model.name, "CarMake": car_model.car_make.name})
+    for car_model in car_models: 
+        cars.append(
+            {
+                "CarModel": car_model.name,
+                "CarMake": car_model.car_make.name
+            }
+        )
     return JsonResponse({"CarModels":cars})
 
 # # Update the `get_dealerships` view to render the index page with
